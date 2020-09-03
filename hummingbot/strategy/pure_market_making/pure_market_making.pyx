f46cd5626bfd3965135e5ce7af89cb24a112c758
bennyliaw <bennyliaw@gmail.com>
2020-09-03 17:23:08 +0800

    Update pure_market_making.pyx

--- hummingbot/strategy/pure_market_making/pure_market_making.pyx
+++ hummingbot/strategy/pure_market_making/pure_market_making.pyx
@@ -810,7 +810,7 @@ cdef class PureMarketMakingStrategy(StrategyBase):
                     [f"  Profitable filter shift SELL orders by {adjust:.6g}."]
                 )
         if len(proposal.buys) >= 1 and len(self._sell_trades) >= 1:
-            if proposal.buys[0].price > self._sell_trades[-1].price * (1 - self._min_profitability):
+            if proposal.buys[0].price > Decimal(self._sell_trades[-1].price) * (1 - self._min_profitability):
                 adjust = Decimal(proposal.buys[0].price) - Decimal(self._sell_trades[-1].price) * (1 - self._min_profitability)
 
                 self.logger().info(f"BUY Order unprofitable, buy price: {proposal.buys[0].price:.6g} vs last sell: {self._sell_trades[-1].price:.6g}, will shift={adjust:.6g}")
