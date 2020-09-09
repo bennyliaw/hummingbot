f963d00ab221d990f1572af3072e817a2b42e9ab
bennyliaw <bennyliaw@gmail.com>
2020-09-09 11:11:51 +0800

    Update vol_tracker_script.py

--- scripts/vol_tracker_script.py
+++ scripts/vol_tracker_script.py
@@ -44,14 +44,12 @@ class VolTracker(ScriptBase):
         # If the bot just got started, we'll not have these numbers yet as there is not enough mid_price sample size.
         # We'll start to have these numbers after interval * long_term_period (150 seconds in this example).
 
-        diff = self.prev_vol is not None and avg_short_volatility is not None and abs(avg_short_volatility - self.prev_vol)
+        diff = self.prev_vol is not None and avg_short_volatility is not None and abs(avg_short_volatility - self.prev_vol) or None
         if avg_short_volatility is not None:
             self.prev_vol = avg_short_volatility
 
-        if diff and diff < 0.0002 :
+        if diff is not None and diff <= 0.0001 :
             return
-
-        self.log(f"avg_short_volatility: {avg_short_volatility} prev_vol={self.prev_vol}")
 
         if avg_short_volatility is None or median_long_volatility is None:
             self.log(f"avg_short_volatility: {avg_short_volatility} median_long_volatility: {median_long_volatility}")
